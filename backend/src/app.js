13ef45434ecaf6867aa0cfedefd02edab23708fc
Jason Cheng <jasoncheng224@gmail.com>
2022-01-23 22:52:04 -0800

    installed environment management package and changed port

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,11 @@
+const readenv = require("dotenv").config();
+
+if (readenv.error)
+    throw "You need a .env file! An empty one is fine for now.";
+
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 8000;
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
