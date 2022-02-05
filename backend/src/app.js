9d22c587b9d9d2c94707938ecd92e1bcf3104d04
Jason Cheng <jasoncheng224@gmail.com>
2022-02-04 20:49:26 -0800

    installed mongoose

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,10 @@
+const readenv = require("dotenv").config();
+
+if (readenv.error)
+    throw "You need a .env file!";
+
 const express = require("express");
-const db = require("./db.js");
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 8000;
@@ -10,7 +15,8 @@ app.get("/", (req, res) => {
 
 // Main function - executed immediately
 (async () => {
-    await db.connect();
+    await mongoose.connect(process.env.DB_URL, { dbName: process.env.DB_NAME });
+
     app.listen(port, () => {
         console.log(`Example app listening on port ${port}`);
     });
