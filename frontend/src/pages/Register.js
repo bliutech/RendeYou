e46722ef95f11f59caaee872e70082f346d69f82
Benson Liu <bensonhliu@gmail.com>
2022-02-12 15:50:07 -0800

    Benson/nav bar (#8)
    * Catchup from main (#7)
    
    * backend: set up MongoDB (#4)
    
    * installed environment management package and changed port
    
    * mongodb setup
    
    * merge mistake
    
    * installed mongoose (#5)
    
    Co-authored-by: Jason Cheng <jasoncheng224@gmail.com>
    
    * added basic Navbar.js design, components/pages file organization structure, and BrowserRouter for multiple pages
    
    * fixed duplicate const declaration
    
    * rm'd a \n
    
    Co-authored-by: Jason Cheng <jasoncheng224@gmail.com>
    Co-authored-by: Do3naut <51458829+Do3naut@users.noreply.github.com>

---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Login()
+{
+    return(
+        <div>
+            <h1> Register </h1>
+            <p> Register your RendeYou account. </p>
+        </div>
+    );
+}
