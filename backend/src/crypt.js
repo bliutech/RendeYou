fd0285a18cd0f4dbbd07b4cb83d89e68b185c7ae
Jason Cheng <jasoncheng224@gmail.com>
2022-02-12 14:47:28 -0800

    backend: implement /register endpoint (#6)
    * test user schema
    
    * quote style (minor)
    
    * install cors
    
    * working post endpoint
    
    * working registration and db write
    
    * comments
    
    * experimental sessions
    
    * create session on registration
    
    * organize requires
    
    * ascii dividers
    
    * username/password validation
    
    * grammar
    
    * subject-verb agreement
    
    * empty line
    
    * removed unneeded cookie-parser package
    
    * minor variable naming/comments
    
    * reject usernames with whitespace

---
+++ backend/src/crypt.js
@@ -0,0 +1,16 @@
+/*
+Util functions for cryptography
+
+Recommended values per https://en.wikipedia.org/wiki/PBKDF2#Purpose_and_operation
+Iterations: at least 1000
+Salt length: 8-16 bytes
+
+More on crypto: https://nodejs.org/api/crypto.html
+*/
+
+const crypto = require("crypto");
+
+exports.hash = (password, salt) =>
+    crypto.pbkdf2Sync(password, salt, 1000, 64, "sha256").toString("hex");
+
+exports.genSalt = () => crypto.randomBytes(16).toString("hex");
