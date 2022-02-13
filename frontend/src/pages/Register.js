17b275acc507911a69f919b6bbef7ebcffbaf329
Do3naut <51458829+Do3naut@users.noreply.github.com>
2022-02-12 21:02:27 -0800

    Displays error message upon register error

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from '../components/Form.js';
 import backend from '../components/Util.js';
 import '../index.css';
 
 export default function Login()
 {
-
-    function handleSubmit(uname, pass) {
-        // Temp implementation: logs the output of uname and pass
+    const [err_msg, setErrMsg] = useState("");
+    async function handleSubmit(uname, pass) {
         const data = {
             username: uname,
             password: pass
         }
-        fetch(backend("/register"), {
+        const res = await fetch(backend("/register"), {
             method: "POST",
             headers: {
             "Content-Type": "application/json"
             },
             body: JSON.stringify(data)
         });
+        const res_j = await res.json();
+        if (res.status >= 400) {
+            setErrMsg(res_j.reason);
+        } else {
+            setErrMsg("");
+        }
     }
-
+    console.log(err_msg);
     return (
         <div className='content'>
             <h1> Register </h1>
             <p> Register your RendeYou account. </p>
+            <p style={{color: "#ff0000"}}>{err_msg}</p>
             <Form onSubmit={handleSubmit}/>
         </div>
     );
