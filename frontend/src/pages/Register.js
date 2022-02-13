991718bd7aa588c47d5157dd938b5c0d073995c1
Do3naut <51458829+Do3naut@users.noreply.github.com>
2022-02-12 17:47:11 -0800

    Form.js functional
    Form.js contains the class Form, which may be helpful for implementing username and password input

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import Form from '../components/Form.js';
 
 export default function Login()
 {
+
+    function handleSubmit(uname, pass) {
+        // Temp implementation: logs the output of uname and pass
+        console.log(uname);
+        console.log(pass);
+    }
+
     return(
         <div>
+            <title>RendeYou - Register</title>
             <h1> Register </h1>
             <p> Register your RendeYou account. </p>
+            <Form onSubmit={handleSubmit}/>
         </div>
     );
 }
