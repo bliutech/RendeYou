8b028625a241d78ce7c6df8cb0e228d2264a0599
Do3naut <51458829+Do3naut@users.noreply.github.com>
2022-02-12 17:59:42 -0800

    Generic implementation of Login interface  (#11)
    * Update test-frontend.html
    
    * Create Form.js
    
    * Form.js functional
    
    Form.js contains the class Form, which may be helpful for implementing username and password input
    
    * reverted to previous version of test-frontend.html
    
    * fixed slash conflict
    
    Co-authored-by: Benson Liu <bensonhliu@gmail.com>

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import Form from '../components/Form.js';
 import '../index.css';
 
 export default function Login()
 {
+
+    function handleSubmit(uname, pass) {
+        // Temp implementation: logs the output of uname and pass
+        console.log(uname);
+        console.log(pass);
+    }
+
     return(
         <div className='content'>
             <h1> Register </h1>
             <p> Register your RendeYou account. </p>
+            <Form onSubmit={handleSubmit}/>
         </div>
     );
 }
