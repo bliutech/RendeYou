2f369d3cb3bffffaede6d5b45f7f80f52ec0f852
Benson Liu <bensonhliu@gmail.com>
2022-02-14 00:28:07 -0800

    frontend: Benson/nav bar (#15)
    * Catchup from main (#7)
    
    * backend: set up MongoDB (#4)
    
    * installed environment management package and changed port
    
    * mongodb setup
    
    * merge mistake
    
    * installed mongoose (#5)
    
    Co-authored-by: Jason Cheng <jasoncheng224@gmail.com>
    
    * added basic Navbar.js design, components/pages file organization structure, and BrowserRouter for multiple pages
    
    * fixed duplicate const declaration
    
    * rm'd a \n
    
    * added NavBar structure and implementation inside App.js
    
    * removed unecessary metadata files from frontend/public & implemented basic stylized NavBar. Moved index.css sourcing into independent pages instead of App.jss.
    
    * updated files, favicon.ico. added additional pages for BrowserRouter in App.js and in /pages. updated index.css and NavBar.module.css with formatting fix.
    
    * added Event & Meeting components and added About.js and Friends.js. Added basic functionality of add event button on NavBar
    
    Co-authored-by: Jason Cheng <jasoncheng224@gmail.com>
    Co-authored-by: Do3naut <51458829+Do3naut@users.noreply.github.com>
    Co-authored-by: jabbapen <71731937+jabbapen@users.noreply.github.com>

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../index.css';
+
+export default function About()
+{
+    document.title = 'About | RendeYou';
+    return(
+        <div className='content'>
+            <h1> About RendeYou </h1>
+            <p> Learn about RendeYou, the best website to rendez-vous with your friends! </p>
+        </div>
+    );
+}
