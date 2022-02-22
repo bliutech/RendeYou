11817768a1c272a003f278b705953b11ce1a068f
Do3naut <51458829+Do3naut@users.noreply.github.com>
2022-02-21 18:19:37 -0800

    Dashboard/css friends list & backend linking (#29)
    * transferring style to .css
    
    * display setup + endpoint linking
    
    Cannot be fully tested until front & backend are linked
    
    * Update FriendsList.js

--- frontend/src/components/FriendsList.js
+++ frontend/src/components/FriendsList.js
@@ -1,64 +1,87 @@
 import React from "react"
-import FriendCard from "./FriendCard.js"
-import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar } from "react-native"
-const FriendsList = () => {
+import classes from "./FriendsList.module.css"
+import backend from "./Util.js"
+
+const FriendsList = async () => {
     const dispList = [
         {
             id: "1782637",
             firstname: "Bobber",
             lastname: "Cob",
             email: "jabbapen@gmail.com",
+            image: 'https://reactnative.dev/img/tiny_logo.png',
         },
         {
             id: "182637",
             firstname: "Bobber",
             lastname: "Cob",
             email: "jabbapen@gmail.com",
+            image: 'https://reactnative.dev/img/tiny_logo.png',
         },
         {
             id: "178237",
             firstname: "Bobber",
             lastname: "Cob",
             email: "jabbapen@gmail.com",
+            image: 'https://reactnative.dev/img/tiny_logo.png',
         },
         {
             id: "178637",
             firstname: "Bobber",
             lastname: "Cob",
             email: "jabbapen@gmail.com",
+            image: 'https://reactnative.dev/img/tiny_logo.png',
+        },
+        {
+            id: "178637",
+            firstname: "Bobber",
+            lastname: "Cob",
+            email: "jabbapen@gmail.com",
+            image: 'https://reactnative.dev/img/tiny_logo.png',
         },
     ];
 
-    const styles = StyleSheet.create({
-        container: {
-          flex: 1,
-          marginTop: StatusBar.currentHeight || 5,
-        },
-        item: {
-          backgroundColor: '#f9c2ff',
-          padding: 20,
-          marginVertical: 8,
-          marginHorizontal: 16,
-        },
-        title: {
-          fontSize: 32,
-        },
-    });
+    //  TODO: Test linking with backend by uncommenting the below code block
 
-    const renderItem = ({item}) => (
-        <FriendCard firstname={item.firstname} lastname={item.lastname} email={item.email}/>
-    );
+    const res = await fetch(backend('/user/me'), {
+        credentials: "include",
+    })
+    const res_j = await res.json();
+    const IDList = res_j.friends;
+    let str = IDList.join();
+    console.log(str);  // Debugging
+    dispList = await fetch(backend('/user'), {
+        ids: str,
+    })
 
-    return (
-      <>
-      <SafeAreaView style={styles.container}>
-      <FlatList
-      data={dispList}
-      renderItem={renderItem}
-      />
-      </SafeAreaView>
-      </>
+    return (<>{
+        dispList.map(
+          person => {
+            return (
+              <tr>
+                <td><img className={classes.image} src={person.image} /></td>
+                <td style={{textAlign:"left"}}>
+                    <div className={classes.subitem}>
+                        <p className={classes.largebody}>{person.firstname} {person.lastname}</p>
+                        <p className={classes.bodytext}>{person.email}</p>
+                    </div>
+                </td>
+              </tr>
+            );
+          }
+        )
+    }
+    </>
     );
+    // return renderItem(dispList[0]);
+    //   <>
+    //   <SafeAreaView style={styles.container}>
+    //   <FlatList
+    //   data={dispList}
+    //   renderItem={renderItem}
+    //   />
+    //   </SafeAreaView>
+    //   </>
   }
   
   export default FriendsList;
