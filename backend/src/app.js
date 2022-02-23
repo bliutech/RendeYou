f7393b9e5c40efc130658af1b4b4518dff309c01
Benson Liu <bensonhliu@gmail.com>
2022-02-23 15:21:13 -0800

    catchup with main (#33)
    * Added functionality for login/logout
    
    * added API Enpoints for getting events and updating events
    
    * added endpoint for deleting events
    
    * added endpoint for creating a new event
    
    * added endpoint for creating a new event
    
    * login/logout functional on test-frontend.html
    
    * removed hello world GET reequest
    
    * backend: get, write, and delete currently logged in user (#22)
    
    * get/put/delete current user (wip)
    
    * fixed req.session.id issue
    
    * fixed test html
    
    * strip search results
    
    * get/put/delete current user working; not validated!
    
    * commit before merge
    
    * user input validation (#19)
    
    * get/put/delete current user (wip)
    
    * some user edit validations
    
    very dangerous to allows users to edit their profile in a syntactically incorrect manner
    
    * changed error status code for ID mismatch
    
    * userId
    
    Co-authored-by: Jason Cheng <jasoncheng224@gmail.com>
    
    * some validations
    
    * dont use object.assign because it could overwrite passwordHash and im too lazy to check
    
    * warning
    
    * ignore changes to unwanted fields
    
    * some changes to test-frontend
    
    Co-authored-by: Do3naut <51458829+Do3naut@users.noreply.github.com>
    
    * Outlined structure of README (#17)
    
    * Outlined structure of README
    
    outlined detailed structure of README to match specifications for project
    
    * fixed incorrect typo for Markdown images to hyperlinks
    
    * fixed RendeYou logo image
    
    * changed login/logout to deal with password hashs
    
    * formatting (#24)
    
    * fixed image sources
    
    * backend: check session endpoint (#25)
    
    * check-session
    
    * 5 min instead of 1h
    
    * shorter
    
    * shorter
    
    * credentials true (#26)
    
    * Added events in temporary frontend testing - making a new event tested and works
    
    * Added event schema as well
    
    * Made the recommended cachanges to /event/new
    
    * Added GET request for an event id (/event/:id) and changed /event/new so that it stores the right id variable
    
    * Added GET request for an event id (/event/:id) and changed /event/new so that it stores the right id variable pt 2
    
    * fixed error where the event id is not added to the host's event id array
    
    * Removed /event/delete before merging
    
    Co-authored-by: Rishi Khurana <rishikhurana2@gmail.com>
    Co-authored-by: Jason Cheng <jasoncheng224@gmail.com>
    Co-authored-by: Do3naut <51458829+Do3naut@users.noreply.github.com>

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,12 +10,12 @@ const app = express();
 const port = 8000;
 const cors = require("cors");
 
-const { User } = require("./schemas");
-const { stripUser, escapeRegex } = require("./util");
+const { User, Event } = require("./schemas");
+const { stripUser, escapeRegex, emailRegex } = require("./util");
 const { hash, genSalt } = require("./crypt");
 
 const session = require("express-session");
-const sessionLifetime = 1000 * 60 * 60; // 1h
+const sessionLifetime = 1000 * 60 * 5; // 5 min
 
 //==============================================================================
 // Express app settings
@@ -23,11 +23,14 @@ const sessionLifetime = 1000 * 60 * 60; // 1h
 
 // Allow CORS
 // TODO: Currently allows requests from any origin - change before production!
-let corsOptions;
+const corsOptions = {
+    origin: "*",
+    credentials: true
+};
 if (process.env.ENV == "dev") {
-    corsOptions = { origin: "http://localhost:3000" };
+    corsOptions.origin = "http://localhost:3000"
 } else if (process.env.ENV == "production") {
-    // Production options
+    corsOptions.origin = "PRODUCTION IP HERE"
 }
 app.use(cors(corsOptions));
 
@@ -40,6 +43,7 @@ app.use(session({
     secret: process.env.SESSION_SECRET,
     saveUninitialized: true,
     resave: false,
+    rolling: true,
     cookie: { maxAge: sessionLifetime }
 }));
 
@@ -49,6 +53,8 @@ app.use(session({
 app.get("/", (req, res) => {
     res.sendFile("test-frontend.html", { root: "." });
 });
+
+app.get("/check-session", (req, res) => res.json(Boolean(req.session.userId)));
 
 app.post("/register", async (req, res) => {
     const username = req.body.username;
@@ -90,6 +96,33 @@ app.post("/register", async (req, res) => {
     res.send(stripUser(newUser.toObject()));
 });
 
+app.post("/login", async (req, res) => {
+    // get the username and password of the user trying to login
+    const username = req.body.username;
+    const password = req.body.password;
+    // retrieve that user from database
+    const user = await User.findOne({
+        username: username,
+    }).lean();
+    if (user && user.passwordHash == hash(password, user.salt)) {
+        req.session.userId = user._id; // then create a new session ID and return that user
+        res.send(stripUser(user));
+    } else { // otherwise, return an error status
+        res.sendStatus(403);
+    }
+});
+
+app.post("/logout", async (req, res) => {
+    const id = req.session.userId;
+    const user = await User.findById(id).lean();
+    if (user) {
+        req.session.destroy();
+        res.send();
+    } else {
+        res.sendStatus(409); // 409 Conflict
+    }
+});
+
 app.get("/user", async (req, res) => {
     let users = User.find();
 
@@ -123,29 +156,110 @@ app.get("/user", async (req, res) => {
     }
 
     users = await users.lean();
-
+    users.map(user => stripUser(user));
     res.send(users);
 });
 
-app.get("/user/:id", async (req, res) => {
+app.get("/user/:id([0-9a-f]{24})", async (req, res) => {
     const id = req.params.id;
-
-    // Validate id
-    if (!/[0-9a-f]{24}/.test(id)) {
-        res.status(400); // 400 Bad Request
-        res.send({ error: "id is not a 24-digit hexadecimal string" });
-        return;
-    }
 
     // .lean() makes the query return a JS object instead of a document
     const user = await User.findById(id).lean();
     if (user) {
         res.send(stripUser(user));
     } else {
-        res.status(404); // 404 Not Found
-        res.send();
+        res.sendStatus(404); // 404 Not Found
     }
 });
+
+app.get("/user/me", checkAuth, async (req, res) => {
+    const id = req.session.userId;
+
+    // .lean() makes the query return a JS object instead of a document
+    const user = await User.findById(id).lean();
+    if (user) {
+        res.send(stripUser(user));
+    } else {
+        res.sendStatus(404); // 404 Not Found
+    }
+});
+
+// Doesn't allow users to change their username or password
+app.put("/user/me", checkAuth, async (req, res) => {
+    const id = req.session.userId;
+    // Find the user indicated by the session's userId, i.e. the logged in user
+    const user = await User.findById(id);
+    const update = req.body;
+
+    if (update.email && !emailRegex.test(update.email)) {
+        res.status(400);
+        res.send({ error: "Invalid email" });
+        return;
+    }
+
+    user.email = update.email;
+    user.firstName = update.firstName;
+    user.lastName = update.lastName;
+
+    await user.save();
+    res.send();
+});
+
+app.delete("/user/me", checkAuth, async (req, res) => {
+    await User.findByIdAndDelete(req.session.userId);
+    req.session.destroy();
+    res.send();
+});
+
+//==============================================================================
+// Route handlers
+
+// Automatically sends a 403 if the user isn't logged in, so your route handler
+// can be assured that the user is authenticated.
+// Add this as an intermediate handler to any endpoint that requires
+// authentication.
+function checkAuth(req, res, next) {
+    if (req.session.userId)
+        next();
+    else
+        res.sendStatus(403); // 403 Forbidden
+}
+app.post("/event/new", checkAuth, async(req, res) =>  {
+    //need to make sure the changes are valid (i.e. the date is valid, etc.)
+    const title = req.body.title;
+    const id = req.session.userId;
+    const user = await User.findById(req.session.userId).lean();
+    if (!title) {
+        res.status(400);
+        res.send({error: "Event name is empty"});
+        return;
+    }
+    const newEvent = new Event(req.body);
+    newEvent.host = id;
+    try {
+        await newEvent.save();
+        await User.findOneAndUpdate(
+            {_id: id},
+            {$push: {hostedEvents: newEvent.id}}
+        );
+        res.send();
+    } catch(err) {
+        res.status(403);
+        res.send();
+    }
+ });
+
+ app.get("/event/:id([0-9a-f]{24})", async(req, res) => {
+   const eventID = req.params.id;
+   const event = await Event.findById(eventID).lean();
+   if (event) { //make sure the event is valid, and if it is, send it
+       res.send(event);
+   } else { //otherwise, send a 404 error
+       res.sendStatus(404);
+   }
+});
+
+ 
 
 //==============================================================================
 
@@ -155,6 +269,7 @@ async function main() {
     app.listen(port, () => {
         console.log(`Example app listening on port ${port}`);
     });
+
 }
 
 main();
