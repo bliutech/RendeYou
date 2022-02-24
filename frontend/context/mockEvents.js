92db953dccf8d3b08f9f8256241f044ba6d89489
Connor Pedersen <jabbapen@gmail.com>
2022-02-23 16:46:02 -0800

    Added some mock events for testing purposes

---
+++ frontend/context/mockEvents.js
@@ -0,0 +1,32 @@
+const mockEvents = [
+  {
+    id: 'cobber_1',
+    date: 1519211809934,
+    title: 'Superbowl',
+    description: 'Match between Rams and Bengals',
+    time: '1200',
+    location: 'Bruin Walk',
+    host: 'bill cob',
+    members: ['randall guy', 'willy wonka'],
+  },
+  {
+    id: 'cobber_2',
+    date: 1519211809935,
+    title: 'lunch',
+    description: 'cheesesteak',
+    time: '2400',
+    location: 'Willys',
+    host: 'bill_cob',
+    members: [],
+  },
+  {
+    id: 'randall_1',
+    date: 1519211809935,
+    title: 'dinner',
+    description: 'icecream',
+    location: 'Konkers',
+    host: 'randall_guy',
+    members: ['bill_cob'],
+  },
+]
+export default mockEvents
