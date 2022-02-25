417bc6c31573249145f2b55a7013ad8180fcda41
Connor Pedersen <jabbapen@gmail.com>
2022-02-24 19:43:32 -0800

    Added some event stuff as well as some profile stuff

---
+++ frontend/src/components/EventCard.js
@@ -0,0 +1,42 @@
+const EventCard = ({ event, joinHandler, ...attributes }) => {
+  const date = new Date(event.date)
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }
+  return (
+    <>
+      <div>
+        <div>
+          <p>{event.title}</p>
+          <p>{event.host}</p>
+        </div>
+        <div>
+          <div>
+            <p>{event.description}</p>
+          </div>
+          <div>
+            <p>{date.toLocaleDateString('en-US', options)}</p>
+            <p>
+              {date.toLocaleTimeString('en-US', {
+                hour: '2-digit',
+                minute: '2-digit',
+              })}
+            </p>
+          </div>
+        </div>
+        <div>
+          <button onClick={joinHandler}>Join</button>
+          <p>{event.members.join(' ')}</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default EventCard
+//TODO: make this adapt to users timezone
+//TODO: link host to profile
+//TODO: talk to backend about date format
---
+++ frontend/src/components/ProfileCard.js
@@ -0,0 +1,16 @@
+const ProfileCard = (user) => {
+  return (
+    <>
+      <img src={user.picture} alt='' width='100px' />
+      <p>{user.firstName + ' ' + user.lastName}</p>
+      <br />
+      <p>{user.email}</p>
+      <br />
+      <p>{user.username}</p>
+      <br />
+      <p>{user.friends.join(' ')}</p>
+    </>
+  )
+}
+
+export default ProfileCard
