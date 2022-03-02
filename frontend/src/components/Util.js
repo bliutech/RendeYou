672b400865b200850a0f467de69d509de087b199
Connor Pedersen <jabbapen@gmail.com>
2022-03-01 22:43:08 -0800

    Checksession missing a }. Also changed from post to get since it is get in backend

--- frontend/src/components/Util.js
+++ frontend/src/components/Util.js
@@ -19,33 +19,31 @@ export async function getUserData() {
 }
 
 // returns true if session is active, returns false if session has expired
-export async function checkSession()
-{
-    const res = await fetch(backend("/check-session"), {
-        method: "POST",
-        credentials: "include",
-        headers: {
-        "Content-Type": "application/json"
-        }
-    });
-    const res_j = await res.json();
-    return res_j;
+export async function checkSession() {
+  const res = await fetch(backend('/check-session'), {
+    method: 'GET',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  const res_j = await res.json()
+  return res_j
+}
 
 // formats date string to confirm with React's yyyy-MM-dd format
 export function formatDate(date_str) {
-    let date = new Date(date_str);
-    let month = '' + (date.getMonth() + 1);
-    let day = '' + date.getDate();
-    let year = date.getFullYear();
+  let date = new Date(date_str)
+  let month = '' + (date.getMonth() + 1)
+  let day = '' + date.getDate()
+  let year = date.getFullYear()
 
-    if (month.length < 2)
-    {
-        month = '0' + month;
-    }  
-    if (day.length < 2) 
-    {
-        day = '0' + day;
-    }
-    
-    return [year, month, day].join('-');
+  if (month.length < 2) {
+    month = '0' + month
+  }
+  if (day.length < 2) {
+    day = '0' + day
+  }
+
+  return [year, month, day].join('-')
 }
