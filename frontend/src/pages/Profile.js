ef3f835b53e8e85cec04df37c9cd1d9c056b19df
Connor Pedersen <jabbapen@gmail.com>
2022-03-03 17:19:23 -0800

    Profile works now though needs some better css

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,15 +1,30 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import '../index.css'
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import '../index.css';
 import ProfileCard from '../components/ProfileCard.js';
+import { UserDataContext } from '../context/UserDataProvider';
 
 export default function Profile() {
   document.title = 'Profile | RendeYou';
+  const { user } = useContext(UserDataContext);
+  console.log(user);
+
   return (
     <>
-      <h1>Profile Page</h1>
-      <ProfileCard/>
-      <Link to='/'>Home</Link>
+      <div
+        style={{
+          position: 'fixed',
+          bottom: '300px',
+          right: '600px',
+          width: '300px',
+          border: '3px solid #fc6a01',
+          textAlign: 'center',
+        }}
+      >
+        <h1>Profile Page</h1>
+        <ProfileCard user={user} />
+        <Link to='/'>Home</Link>
+      </div>
     </>
-  )
+  );
 }
