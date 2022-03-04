c0d792f911751e415769fb18f6a39567465bc678
Connor Pedersen <jabbapen@gmail.com>
2022-03-03 16:36:56 -0800

    Revised context implementation also deleted extra files

--- frontend/src/context/UserDataProvider.js
+++ frontend/src/context/UserDataProvider.js
@@ -1,15 +1,38 @@
-import React from 'react'
-import { useState } from 'react'
+import React from 'react';
+import { useState } from 'react';
+import backend from '../components/Util';
 
-const UserDataContext = React.createContext()
+const UserDataContext = React.createContext();
 
 const UserDataProvider = ({ children }) => {
-  const [user, setUser] = useState(false)
+  const [user, setUser] = useState();
+  const [isLoggedin, setisLoggedin] = useState(false);
+  // const setUser = async (currUser) => {
+  //   const res = await fetch(backend('/user/me'), {
+  //     method: 'PUT',
+  //     credentials: 'include',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify(currUser),
+  //   });
+  //   if (res.status >= 400) {
+  //     console.log();
+  //   }
+  //   updateUser(currUser);
+  // };
   return (
-    <UserDataContext.Provider value={{ user, setUser }}>
+    <UserDataContext.Provider
+      value={{
+        user,
+        setUser,
+        isLoggedin,
+        setisLoggedin,
+      }}
+    >
       {children}
     </UserDataContext.Provider>
-  )
-}
+  );
+};
 
-export { UserDataContext, UserDataProvider }
+export { UserDataContext, UserDataProvider };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App.js'
-import { LoginProvider, LoginContext } from './context/LoginProvider.js'
-import { UserDataProvider } from './context/UserDataProvider.js'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+import { UserDataProvider } from './context/UserDataProvider.js';
 ReactDOM.render(
   <React.StrictMode>
     <UserDataProvider>
-      <LoginProvider>
-        <App />
-      </LoginProvider>
+      <App />
     </UserDataProvider>
   </React.StrictMode>,
   document.getElementById('root')
-)
+);
