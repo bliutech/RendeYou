a877b89ff3f6749d160d78e70eace81cc6147699
Connor Pedersen <jabbapen@gmail.com>
2022-03-06 03:20:52 -0800

    Shows friend events

--- frontend/src/pages/Meetings.js
+++ frontend/src/pages/Meetings.js
@@ -6,7 +6,7 @@ import { getEvent, getUsers } from '../components/Util';
 import EventCard from '../components/EventCard';
 import HostEventCard from '../components/HostEventCard';
 import backend from '../components/Util';
-import { deleteEvent } from '../components/Util';
+import { deleteEvent, getFriend } from '../components/Util';
 
 export default function Meetings() {
   document.title = 'Meetings | RendeYou';
@@ -35,8 +35,20 @@ export default function Meetings() {
 
   const getFriendEvents = async () => {
     const ids = user.friends;
-    const friends = await getUsers(ids);
+    let friends = [];
+    for (let i = 0; i < ids.length; i++) {
+      friends.push(await getFriend(ids[i]));
+    }
     console.log(friends);
+    let events = [];
+    for (let i = 0; i < friends.length; i++) {
+      for (let j = 0; j < friends[i].hostedEvents.length; j++) {
+        const addedEvent = await getEvent(friends[i].hostedEvents[j]);
+        events.push(addedEvent);
+      }
+    }
+    console.log(events);
+    setFriendEvents(events);
   };
 
   const deleteHandler = async (id) => {
@@ -52,6 +64,23 @@ export default function Meetings() {
         {hostedEvents?.length != 0 ? (
           hostedEvents?.map((e) => {
             return <HostEventCard event={e} deleteHandler={deleteHandler} />;
+          })
+        ) : (
+          <p>No Events</p>
+        )}
+      </div>
+      <h1>Friend Events</h1>
+      <div>
+        {friendEvents.length != 0 ? (
+          friendEvents.map((e) => {
+            return (
+              <EventCard
+                event={e}
+                joinHandler={() => {
+                  alert('do join');
+                }}
+              />
+            );
           })
         ) : (
           <p>No Events</p>
