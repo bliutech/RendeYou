cb783d85322eb35e58a513460c36cf2a31545aa4
Jason Cheng <jasoncheng224@gmail.com>
2022-03-07 18:50:13 -0800

    CSS for event cards (#71)

--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -1,5 +1,5 @@
 const settings = {
-    ENV: "dev", // If you want to use the deployed backend, change this to "production"
+    ENV: "production", // If you want to use the deployed backend, change this to "production"
     BACKEND_IP: "http://165.232.135.214:8000"
 }
 
