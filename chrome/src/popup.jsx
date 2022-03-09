b08cccd9f040b3ca28076540ceff94e152659820
Do3naut <51458829+Do3naut@users.noreply.github.com>
2022-03-08 20:22:48 -0800

    for production build
    very scuffed

--- chrome/src/popup.jsx
+++ chrome/src/popup.jsx
@@ -11,21 +11,30 @@ function PopUp() {
     const [dispList, updateDispList] = useState([]);
     const [err_msg, setErrMsg] = useState('');
     const [signal, setSignal] = useState(0);
-    // const [nextEvent, setNextEvent] = useState(" "); 
-    // const [nextEventTime, setEventTime] = useState(Date.now());
+    const [loggedIn, setLogin] = useState(false);
+    const [notified, setNotified] = useState(false);
+    const [uname, setUname] = useState(""); 
+    const [pass, setPass] = useState("");
     let nextEvent = " ";
     let nextEventTime = Date.now();
+    let cookieVal = "";
     console.log("Initial time: " + Date.now());
     useEffect(async () => {
+        chrome.cookies.get({"url": "http://165.232.135.214:8000", "name": "connect.sid"}, (cookie) => {
+            console.log(cookie);
+            cookieVal = cookie.value;
+        });
         const res = await fetch(backend('/user/me'), {
             method: 'GET',
             credentials: 'include',
         });
         if (res.status >= 400) {  // Only error is 403 Forbidden - Not logged in
             setErrMsg("Login to receive your events!");
+            setLogin(false);
             return;
         }
         let res_j = await res.json();
+        setLogin(true);
         setErrMsg("Welcome back, " + res_j.firstName + "!");
         const eventList = res_j.hostedEvents.concat(res_j.subscriptions);
         let dispEvents = [];
@@ -44,13 +53,12 @@ function PopUp() {
                 nextEventTime = event.date;
                 nextEvent = event.title;
                 console.log("Set alert for event: " + event.title + "for time: " + event.date);
+                setNotified(false);
             } else if (event.date > Date.now() && nextEventTime > event.date) {
                 nextEventTime = event.date;
                 nextEvent = event.title;
                 console.log("Set alert for event: " + event.title + "for time: " + event.date);
-                // setEventTime(event.date);
-                // setNextEvent(event.title);
-                // console.log("Set alert for event: " + event.title + "for time: " + event.date);
+                setNotified(false);
             }
             const date = new Date(event.date);
             const options = {
@@ -64,10 +72,8 @@ function PopUp() {
                 minute: '2-digit',
             });
             event.date = datestr;
-            console.log(event);
             dispEvents.push(event);
         }
-        console.log(dispEvents);
         updateDispList(dispEvents);
         setInterval(async () => {
             const res = await fetch(backend('/user/me'), {
@@ -80,32 +86,85 @@ function PopUp() {
             setSignal(Date.now() % 1000);
             console.log("Checking time... Next event: " + nextEventTime);
             console.log(nextEvent);
-            if (Date.now() + 601000 > nextEventTime) {  // Not handling when event time is past now. 
+            if (Date.now() + 601000 > nextEventTime && !notified) {  // Not handling when event time is past now. 
                 window.alert(nextEvent + " is happening in less than 10 minutes!");
+                setNotified(true);
             }
-        }, 600000)  // Temporarily set to 10 minutes
+        }, 10000)  // Refresh after 10 seconds
     }, [signal]);
 
 
-    return(
-    <div className={classes.frame}>
+    async function handleSubmit(uname, pass) {
+        const data = {
+          username: uname,
+          password: pass,
+        };
+        const res = await fetch(backend('/login'), {
+          method: 'POST',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        });
+        if (res.status >= 400) {
+            setErrMsg(res.error);
+        } else {
+            setSignal(Date.now() % 100);
+        }
+    }
+
+
+    if (loggedIn) {
+        return(
+        <div className={classes.frame}>
+            <h1>RendeYour Events</h1>
+            <p className={classes.largebody}>{err_msg}</p>
+            <>{
+            dispList.map(
+            event => {
+                return (
+                    <tr className={classes.subitem}>
+                    <td className={classes.bodytext}>{event.title}</td>
+                    <td className={classes.bodytext2}>{event.date}</td>
+                </tr>
+                );
+            }
+            )
+            }
+        </>
+        </div>
+        );
+    } else {
+        return(
+        <div className={classes.frame}>
         <h1>RendeYour Events</h1>
         <p className={classes.largebody}>{err_msg}</p>
-        <>{
-        dispList.map(
-          event => {
-            return (
-                <tr className={classes.subitem}>
-                <td className={classes.bodytext}>{event.title}</td>
-                <td className={classes.bodytext2}>{event.date}</td>
-              </tr>
-            );
-          }
-        )
-        }
-    </>
-    </div>
-    );
+        <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          handleSubmit(uname, pass)
+        }}
+        >
+        <legend>Username</legend>
+        <input
+          value={uname}
+          onChange={(e) =>{ setUname(e.target.value); }}
+          placeholder='Enter Username'
+        />
+        <legend>Password</legend>
+        <input
+          type='password'
+          value={pass}
+          onChange={(e) =>{ setPass(e.target.value); }}
+          placeholder='Enter Password'
+        />
+        <br />
+        <input type='submit' value='Submit' className={classes.button}/>
+      </form>
+      </div>
+        );
+    }
 }
 
 render(<PopUp />, document.getElementById("react-target"));
