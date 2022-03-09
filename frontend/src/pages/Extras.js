4bfced75cf64f470a038d36d5d677a8f878aae36
bliutech <bensonhliu@gmail.com>
2022-03-09 02:01:44 -0800

    fixed some css for extras page

--- frontend/src/pages/Extras.js
+++ frontend/src/pages/Extras.js
@@ -12,15 +12,16 @@ export default function Extras() {
   }, []);
   document.title = 'Extras | RendeYou';
   return (
-    <div className='content'>
+    <div className='extras-content'>
       <h1> RendeYou Extras </h1>
       <p>
         {' '}
         Want to enable notifications for RendeYou? Download our Chrome extension! {' '}
       </p>
       <p>Enjoy a portable, simplistic list of all your events just a click away!</p>
-      <p>Instructions: Open chrome and navigate to `chrome://extensions`. Turn on developer mode at the top right of the screen, and click on Load Unpacked in the toolbar that should have just displayed. Navigate to the unzipped file and select the dist folder to load into Chrome.</p>
-      <p>Download RendeYour Events <a href={ZipFile} className='this-a'>here</a>.</p>
+      <h2>Instruction</h2>
+      <p>Open chrome and navigate to `chrome://extensions`. Turn on developer mode at the top right of the screen, and click on Load Unpacked in the toolbar that should have just displayed. Navigate to the unzipped file and select the dist folder to load into Chrome.</p>
+      <p>Download RendeYour Events <a href={ZipFile} className='extras-a'>here</a>.</p>
     </div>
   );
 }
