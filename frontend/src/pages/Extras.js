9dc5c6e97cb70fa1291ffd5d67551749e95b448d
bliutech <bensonhliu@gmail.com>
2022-03-09 00:29:46 -0800

    implemented download for RendeYourEvents Chrome Extension and fixed some css

--- frontend/src/pages/Extras.js
+++ frontend/src/pages/Extras.js
@@ -3,6 +3,7 @@ import '../index.css';
 import { UserDataContext } from '../context/UserDataProvider';
 import { useContext, useEffect } from 'react';
 import './Extras.css';
+import ZipFile from '../files/RendeYourEvents.zip';
 
 export default function Extras() {
   const { updateData } = useContext(UserDataContext);
@@ -17,6 +18,7 @@ export default function Extras() {
         {' '}
         Want to enable notifications for RendeYou? Download our Chrome extension! {' '}
       </p>
+      <p>Download RendeYour Events <a href={ZipFile} className='this-a'>here</a>.</p>
     </div>
   );
 }
