cd574334243da0b1ee188b191b0f9f0b13d9b73c
bliutech <bensonhliu@gmail.com>
2022-03-08 17:37:43 -0800

    implemented add/remove friend on the dynamic profile page for each user

--- frontend/src/pages/GetUser.js
+++ frontend/src/pages/GetUser.js
@@ -1,36 +1,63 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import React, { useEffect, useState, useContext } from 'react';
+import { useParams } from 'react-router-dom';
 import '../index.css';
 import ProfileCard from '../components/ProfileCard.js';
-import backend from '../components/Util.js';
-import { getFriend } from '../components/Util.js';
+import { getFriend, addUserData, removeFriend } from '../components/Util.js';
+import { UserDataContext } from '../context/UserDataProvider';
 import './GetUser.css';
 
 export default function GetUser() {
+  const { updateData, user } = useContext(UserDataContext);
   let { id } = useParams('');
-  let [user, setUser] = useState(null);
+  let [fetched_user, setUser] = useState(null);
   async function getUser(id) {
     const curr_user = await getFriend(id);
     setUser(curr_user);
   }
 
+  async function handleAddFriend() {
+    let f_list = user.friends;
+    f_list.push(id);
+    const newUser = {
+      friends: f_list,
+    };
+    await addUserData(newUser);
+    updateData();
+  }
+
+  const deleteFriend = async (id) => {
+    await removeFriend(id);
+    await updateData();
+  };
+
   useEffect(() => {
     getUser(id);
-    console.log(user);
+    console.log(fetched_user);
   }, []);
   document.title =
-    user !== null
-      ? user.username + "'s Profile | RendeYou"
+    fetched_user !== null
+      ? fetched_user.username + "'s Profile | RendeYou"
       : 'User Not Found | RendeYou';
   return (
     <div className='content'>
       <div className='profile'>
         <h1 className='profile-center'>Profile Page</h1>
-        {user !== null ? (
-          <ProfileCard user={user} />
+        {fetched_user !== null ? (
+          <ProfileCard user={fetched_user} />
         ) : (
           <p>{id} is not an existing user ID :(</p>
         )}
+        {fetched_user !== null && user ? (
+          !user.friends.includes(fetched_user.id) ? 
+          (<button onClick={async () => handleAddFriend()} className='button'>
+            Add Friend
+          </button>) : <button onClick={async () => deleteFriend()} className='button'>
+            Remove
+          </button>
+        ) : (
+          null
+        )}
+
       </div>
     </div>
   );
