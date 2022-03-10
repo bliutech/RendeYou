9155159167a506f0e3336f3c3127e37fe62cf524
bliutech <bensonhliu@gmail.com>
2022-03-09 17:05:13 -0800

    quick fix for About page css link

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -20,7 +20,7 @@ export default function About() {
       <h2> What is RendeYou? </h2>
       <p> RendeYou is a social media application focusing on connecting user for instant meet ups!</p>
       <h2> Who developed RendeYou? </h2>
-      <p> RendeYou was developed by the Freshly Poached team! You can see your source code at <a href='https://github.com/bliutech/RendeYou'>link</a>.</p>
+      <p> RendeYou was developed by the Freshly Poached team! You can see your source code at <a href='https://github.com/bliutech/RendeYou' className='about-a'>link</a>.</p>
       <h2> How can I start using RendeYou? </h2>
       <p><Link to='/register'>Register</Link> today to start using RendeYou! </p>
     </div>
