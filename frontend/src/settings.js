4fdedea61aac4446b0d1fa55dfa5a40e0b639816
Jason Cheng <jasoncheng224@gmail.com>
2022-03-11 12:28:53 -0800

    Update settings.js (#102)

--- frontend/src/settings.js
+++ frontend/src/settings.js
@@ -1,6 +1,6 @@
 const settings = {
-  ENV: 'production', // If you want to use the deployed backend, change this to "production"
-  BACKEND_IP: 'https://jcfp.site/app/rendeyou',
+  ENV: "dev", // If you want to use the deployed backend, change this to "production"
+  BACKEND_IP: "https://jcfp.site/app/rendeyou"
 };
 
 export default settings;
